9d77663e405d9fce98e030f79a38080bc62e525c
jaens0797 <jmoras@ucenfotec.ac.cr>
2025-12-03 20:20:20 -0600

    update

--- Grupo 4  Preservacion-lenguas-Indigenas/Back-end/models/diccionario.model.js
+++ Grupo 4  Preservacion-lenguas-Indigenas/Back-end/models/diccionario.model.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
 let schemaDiccionario = new mongoose.Schema({
-    terminoEspanol : { type: String, required: true },
-    terminoLengua : { type: String, required: true },
-    pueblo : { type: String, required: true },
-    audioPronunciacion : { type: String }  // URL opcional
+  terminoEspanol: {
+    type: String,
+    required: true
+  },
+  terminoLengua: {
+    type: String,
+    required: true
+  },
+  pueblo: {
+    type: String,
+    required: true
+  },
+  audioPronunciacion: {
+    type: String
+  }
 });
 
 module.exports = mongoose.model('Diccionario', schemaDiccionario, 'diccionario');
